b907215bcbab2ba396cfd95a83216175ca15f8ed
Olsedyr <56992952+Olsedyr@users.noreply.github.com>
2024-04-09 13:27:52 +0200

    More UI stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -34,7 +34,6 @@ const App = () => {
 
                 <Inventory />
 
-
                 <PrevProd />
 
 
--- frontend/src/components/assemblyInfo.js
+++ frontend/src/components/assemblyInfo.js
@@ -2,10 +2,15 @@ import React, { useState } from 'react';
 
 const AssemblyInfo = () => {
 
+    const [status, setStatus] = useState("Idle"); // placeholder
+
     return (
         <div className="container">
             <div className="assembly-info">
                 <h2>Assembly info: </h2>
+                <div className="battery-container">
+                    <div className="status-text">Assembly status: {status}</div>
+                </div>
             </div>
         </div>
     );
